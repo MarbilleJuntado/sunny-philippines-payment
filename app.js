d4c8240d7a55c6eaae8f97c73184e4b1e2aedac1
Marbille Juntado <marbille.juntado@hotmail.ph>
2018-11-05 19:50:40 +0800

    Make it better

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const bodyParser = require('body-parser');
 //sample routes
 const oauth = require('./routes/oauth');
 
+//transfer route
+const transfer = require('./routes/transfer');
+
 //import settings
 const settings = require('./lib/settings');
 
@@ -45,16 +48,53 @@ app.use(session({
  * GET                  /oauth/redirect
  */
 app.get('/', function(req, res){
+    //create redirect link
+    let redirect_link = `${settings.ubp.url}/convergent/v1/oauth2/authorize?client_id=${settings.ubp.client_id}&scope=${settings.ubp.scope}&response_type=${settings.ubp.request_type}&redirect_uri=${settings.ubp.redirect_uri}`;
+
+    //check if there is session token
+    let has_token = false;
+    if(typeof req.session.token != "undefined"){
+        has_token = true;   //set the token to true
+    }
     //display hello world
     res.render('index', //render /views/index.ejs
         //pass data to index.ejs
         {
             title: 'Index Page',
-            body: 'Hello World'
+            has_token: has_token,
+            redirect_link: redirect_link
         }
     );
 });
 
+app.get('/send', function(req, res) {
+    res.send(`
+        <html>
+            <head>
+                <link rel="stylesheet" href="/css/index.css">
+                <link rel="stylesheet" href="/css/buttons.css">
+            </head>
+            <body>
+                <div class="user">
+                    <header class="user__header">
+                        <h1 class="user__title">
+                            Payment Verified!
+                        </h1>
+                        <br>
+                        <img src="https://img.icons8.com/ios/1600/ok.png" width="128" height="128">
+                        </img>
+                        <br><br>
+                        <a class="button button-royal button-pill button-normal" href="http://aboitiz2018.gigamike.net/member">
+                            Return to Dashboard
+                        </a>
+                    </header>
+                </div>
+            </body>
+        </html>
+    `);
+});
+
 app.use('/oauth', oauth);
+app.use('/transfer', transfer);
 
 module.exports = app;
--- routes/oauth.js
+++ routes/oauth.js
@@ -10,12 +10,45 @@ const ajax = require('../lib/ajax');
 //create /redirect endpoint
 router.get('/redirect', function(req, res, next) {
     //access token will be processed here
-    res.render("oauth", //render /views/oauth.ejs
-        //pass data to oauth.ejs
-        {
-            title: "Oauth Page",
-            body: "Access token goes here"
+    ajax.setOptions({
+        url : settings.ubp.url + '/convergent/v1/oauth2/token', //set the url
+        headers: {  //set the headers
+            'content-type' : '	application/x-www-form-urlencoded',
+            'accept': 'text/html'
+        },
+        method: 'POST', //set request to POST
+        form: { //create the form body of the request
+            client_id: settings.ubp.client_id,          //client ID of Application
+            code: req.query.code,                       //put the code we receive from UnionBank Online Login
+            redirect_uri: settings.ubp.redirect_uri,    //this must be same with the redirect URI we used initially
+            grant_type: settings.ubp.grant_type         //set the grant type to authorization_code
         }
-    );
+    }).post().then(data=>{
+        //if there we got token
+        //save it to session
+        if(typeof data.body.access_token != "undefined"){
+            //save to session
+            req.session.token = data.body.access_token;
+
+            //redirect to transfer form again
+            res.redirect('/');
+        }
+        //if we dont get access token show the body of message
+        res.render("oauth", //render /views/oauth.ejs
+            //pass data to oauth.ejs
+            {
+                title: "Oauth Page",
+                body: data.body
+            }
+        );
+    }).catch(error=>{
+        res.render("oauth", //render /views/oauth.ejs
+            //pass data to oauth.ejs
+            {
+                title: "Oauth Page",
+                body: error
+            }
+        );
+    });
 });
 module.exports = router;
